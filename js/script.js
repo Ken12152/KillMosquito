7750c5d69f1428b8d7b00f12163e471d0720cc27
KenFelipe <kenzo12152@gmail.com>
2020-09-05 23:38:37 -0300

    random size mosquito

--- js/script.js
+++ js/script.js
@@ -9,21 +9,35 @@ function adjustGameScreenSize() {
 }
 
 
-var mosquitoWidth = 50
-var mosquitoHeight = 50
+var mosquitoMaxWidth = 90
+var mosquitoMaxHeight = 90
 
 function insertRandomMosquito() {
-    var insertPosX = Math.floor(Math.random() * (windowWidth - mosquitoWidth))
-    var insertPosY = Math.floor(Math.random() * (windowHeight - mosquitoHeight))
+    var insertPosX = Math.floor(Math.random() * (windowWidth - mosquitoMaxWidth))
+    var insertPosY = Math.floor(Math.random() * (windowHeight - mosquitoMaxHeight))
 
     var mosquito = document.createElement('img')
 
     mosquito.src = 'img/mosca.png'
-    mosquito.className = 'mosquito'
+    mosquito.className = randomMosquitoSize()
+    mosquito.style.position = 'absolute'
     mosquito.style.top = insertPosY + 'px'
     mosquito.style.left = insertPosX + 'px'
     
     document.body.appendChild(mosquito)
 
     console.log(insertPosY, insertPosX)
+}
+
+function randomMosquitoSize() {
+    var classth = Math.floor(Math.random() * 3)
+
+    switch(classth) {
+    case 0:
+        return 'mosquito1'
+    case 1:
+        return 'mosquito2'
+    case 2:
+        return 'mosquito3'
+    }
 }
